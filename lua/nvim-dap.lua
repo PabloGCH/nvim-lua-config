d7c0b0c26d627eabdf5ba4907b70f8c442423241
pablogch <pablochoconi@gmail.com>
2023-10-01 19:58:07 -0300

    Change in tabby config, added csharp debugger dll selection

--- lua/nvim-dap.lua
+++ lua/nvim-dap.lua
@@ -2,14 +2,42 @@
 --local dap = require('dap')
 
 
-
-
-
 local dap = require('dap')
-
 local HOME = os.getenv("HOME")
 local DEBUGGER_LOCATION = HOME .. "/.config/nvim/netcoredbg"
 
+-- FUNCTION TO GET DLL
+-- ===========================0
+-- I actually got this code from help of github copilot, but I only have a general idea of how it works
+function get_dll() --Looks for dlls in ./bin/Debug/net* and lists them
+    -- finds .csproj file in current directory
+    local csproj = vim.fn.globpath(vim.fn.getcwd(), '*.csproj', false, true)[1]
+    -- gets the name of the file from the path (without the extension)
+    local filename = vim.fn.fnamemodify(csproj, ':t:r')
+    --finds all dlls in bin/Debug/net*
+    local dlls = vim.fn.globpath(vim.fn.getcwd() .. '/bin/Debug/', 'net*/*.dll', false, true)
+    if #dlls == 0 then
+        print('No dll found')
+        return
+    end
+    -- filters out the dlls that don't match the current filename
+    dlls = vim.tbl_filter(function(item)
+        return vim.fn.fnamemodify(item, ':t:r') == filename
+    end, dlls)
+    -- cretes a list of filenames from the full paths
+    local choices = {}
+    for _, dll in ipairs(dlls) do
+        local filename = dll
+        table.insert(choices, filename)
+    end
+    -- shows the list of dlls and returns the selected one
+    local choice = vim.fn.inputlist(choices)
+    if choice <= 0 then
+        return
+    end
+    --returns the full path to the dll
+    return dlls[choice]
+end
 
 dap.adapters.coreclr = {
     type = 'executable',
@@ -17,22 +45,20 @@ dap.adapters.coreclr = {
     args = {'--interpreter=vscode'}
 }
 
-
-
 dap.configurations.cs = {
   {
     type = "coreclr",
     name = "launch - netcoredbg",
     request = "launch",
-    --program = get_dll()
-    program = function()
-        return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
-    end,
+    program = get_dll
+    -- The following code is from daps documentation is commented out because it's not very practical
+    -- to write the path to the dll every time you want to debug
+    -- it's commented out because i don't want to loose it
+    --program = function() 
+    --    return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+    --end,
   },
 }
-
-
-
 
 vim.keymap.set('n', '<F5>', ':lua require"dap".continue()<CR>', {noremap = true, silent = true})
 vim.keymap.set('n', '<F10>', ':lua require"dap".step_over()<CR>', {noremap = true, silent = true})
