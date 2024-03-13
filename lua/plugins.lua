c1d6789ba28d0e8f950fcccccfd7ec7de258d497
pablogch <pablochoconi@gmail.com>
2024-03-13 00:08:39 -0300

    Telescope fix

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -44,9 +44,9 @@ return require('packer').startup(function(use)
     use 'hrsh7th/vim-vsnip'
     --TELESCOPE
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        -- or                            , branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
+      'nvim-telescope/telescope.nvim', tag = '0.1.5',
+      -- or                            , branch = '0.1.x',
+      requires = { {'nvim-lua/plenary.nvim'} }
     }
     --RUST SUPPORT
     use 'simrat39/rust-tools.nvim'
